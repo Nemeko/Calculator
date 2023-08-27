0d8897d3b1594bb34579b2a1d18e133c6c6e2c10
Leopoldo <lnnemec@gmail.com>
2023-08-27 12:59:38 +0200

    Versio 1.0, calculos funcionales con teclado

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let display = document.querySelector("#lcd")
 let teclado = document.querySelector(".teclado")
 let numero = [0,0,0]
 let idx = 1
-let operacion = "igual"
+let operador = ""
 
 const alu = {
     sumar(a,b){
@@ -19,160 +19,72 @@ const alu = {
     dividir(a,b){
         return a/b
     },
-    mostrarTecla(valor){
-        console.log("Tecla presionada: " + valor)
-    }
+    calcular(operator,a,b){
+        let resultado;
+        operator == "NumpadAdd" ? resultado = (this.sumar(a,b)):"";
+        operator == "NumpadSubtract" ? resultado = this.restar(a,b):"";
+        operator == "NumpadMultiply" ? resultado = this.multiplicar(a,b):"";
+        operator == "NumpadDivide" ? resultado = this.dividir(a,b):"";
+        return resultado
+    },
 }
 
 
 calculadora.addEventListener("keydown", e => { 
     e.key >= 0 && e.key <= 9 ? numero[idx] += e.key:""//numero.push(e.key) :"";
     numero[idx] = parseFloat(numero[idx])      //convierte el string en numero
-    console.log("Num 1: "+numero[1] + " -- Num 2: "+ numero[2])
-    
-    console.log(e.code)
-    
-    //e.code == "NumpadEnter" ? console.log(numero[2]):""
-    if (e.code == "NumpadEnter"){
-        switch(operacion){
-        case "sumar":
-            
-        case "restar":
-        
-        case "multiplicar":
-        
-        case "dividir":
-        
+    // console.log("Num 1: "+numero[1] + " -- Num 2: "+ numero[2])
+
+    if(e.code == "NumpadAdd" || e.code == "NumpadSubtract" || e.code == "NumpadMultiply" || e.code == "NumpadDivide"){
+        if (operador != e.code && idx ==2 && numero[2] != 0){
+            console.log("--- %c Dentro del IF para mandar operacion al ALU -> idx = "+idx,"color:yellow"); 
+            numero[0] = alu.calcular(operador, numero[1],numero[2])
+            numero[1] = numero[0];
+            numero[2] = 0;
+            operador = e.code
+        }else if (idx ==2 && numero[2] != 0){
+            console.log("--- %c Dentro del IF para mandar operacion al ALU -> idx = "+idx, "color:orange"); 
+            numero[0] = alu.calcular(operador, numero[1],numero[2])
+            numero[1] = numero[0];
+            numero[2] = 0;
+        }else if(idx == 1){
+            idx = 2;
+            console.log("--- %c Dentro del IF idx -> idx = "+idx,"color:lightblue"); 
+            operador = e.code
+        }else{
+            operador = e.code
         }
+        
     }
-    
-
-    if (e.code == "NumpadAdd"){
-        idx = 2
-        numero[0] = alu.sumar(numero[1],numero[2])
-        numero[1] = numero[0]
-        numero[2] = 0
-        console.log("Dentro de Sumar")
+    console.log("Numero 1: "+numero[1]+"  -- Numero 2: "+numero[2]+"  --%c Operador: "+operador,"color:yellow","  -- Buffer: "+numero[0]);
+    if(e.code == "NumpadEnter"){                                        // NO ESTA HACIENDO EL CALCULO PORQUE ENTER NO DISPARA LA FUNCION MATEMATICA
+        numero[0] = alu.calcular(operador, numero[1],numero[2])
+        numero[1] = numero[0];
+        numero[2] = 0;
+        console.log("%c RESULTADO -> "+numero[1],"color:lightgreen")
     }
-    if (e.code == "NumpadSubtract"){
-        idx = 2
-        numero[0] = alu.restar(numero[1],numero[2])
-        numero[1] = numero[0]
-        numero[2] = 0
-        console.log("Dentro de Restar")
-    }
-    
-    
-
-    //e.code == "NumpadSubtract"
-    //e.code == "NumpadMultiply"
-    //e.code == "NumpadDivide"
-    console.log("resultado:  " + numero[0])
 
 })
 
 
-// const mostrarT = (valor) => console.log("Tecla presionada: " + valor );
+// NumpadDivide
+// NumpadMultiply
+// NumpadSubtract
+// NumpadAdd
+// NumpadEnter
 
 
-
-
-// calculadora.addEventListener("keydown", e => {
-//     console.log("Se presiono una tecla: " + e.keyCode)
-//     switch(e.keyCode){
-//     case 96:
-//         mostrarT("0");
-//         numero[idx] += "0";
-//         console.log("Numero: " + numero);
-//         break;
-//     case 97:
-//         mostrarT("1");
-//         numero[idx] += "1";
-//         console.log("Numero: " + numero);
-//         break;
-//     case 98:
-//         mostrarT("2");
-//         numero[idx] += "2";
-//         console.log("Numero: " + numero);
-//         break;
-//     case 99:
-//         mostrarT("3");
-//         numero[idx] += "3";
-//         console.log("Numero: " + numero);
-//         break;
-//     case 100:
-//         mostrarT("4");
-//         numero[idx] += "4";
-//         console.log("Numero: " + numero);
-//         break;
-//     case 101:
-//         mostrarT("5");
-//         numero[idx] += "5";
-//         console.log("Numero: " + numero);
-//         break;
-//     case 102:
-//         mostrarT("6");
-//         numero[idx] += "6";
-//         console.log("Numero: " + numero);
-//         break;
-//     case 103:
-//         mostrarT("7");
-//         numero[idx] += "7";
-//         console.log("Numero: " + numero);
-//         break;
-//     case 104:
-//         mostrarT("8");
-//         numero[idx] += "8";
-//         console.log("Numero: " + numero);
-//         break;
-//     case 105:
-//         mostrarT("9");
-//         numero[idx] += "9";
-//         console.log("Numero: " + numero);
-//         break;
-//     case 106:
-//         mostrarT('*');
-//         break;
-//     case 107:
-//         mostrarT("+");
-//         idx++
-//         numero[idx] = ""
-//         break;
-//     case 109:
-//         mostrarT("-");
-//         break;
-//     case 110:
-//         mostrarT(".");
-//         break;
-//     case 111:
-//         mostrarT("/");
-//         break;
-//     case 13:
-//         mostrarT("Enter");
-//     }
-// })
-
-
-
-
-
-
-
-
-// 103 = 7
-// 104 = 8
-// 105 = 9
-// 100 = 4
-// 101 = 5
-// 102 = 6
-// 97 = 1
-// 98 = 2
-// 99 = 3
-
-// 106 = *
-// 109 = -
-// 107 = +
-// 13 = =
-// 96 = 0
-// 110 = .
-// 111 = /
+// if (e.code == "NumpadAdd"){
+    //     idx = 2
+    //     numero[0] = alu.sumar(numero[1],numero[2])
+    //     numero[1] = numero[0]
+    //     numero[2] = 0
+    //     console.log("Dentro de Sumar")
+    // }
+    // if (e.code == "NumpadSubtract"){
+    //     idx = 2
+    //     numero[0] = alu.restar(numero[1],numero[2])
+    //     numero[1] = numero[0]
+    //     numero[2] = 0
+    //     console.log("Dentro de Restar")
+    // }
