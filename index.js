1b4e3414f48e21c8539f2ab9270b4804112c4bd0
Leopoldo <lnnemec@gmail.com>
2023-09-03 16:23:10 +0200

    Version 2.0, calculos funcionales con teclado y mouse

--- index.js
+++ index.js
@@ -20,65 +20,54 @@ const alu = {
         return a/b
     },
     calcular(operator,numero){
-        operator == "NumpadAdd" ? numero[0] = this.sumar(numero[1],numero[2]):"";
-        operator == "NumpadSubtract" ? numero[0] = this.restar(numero[1],numero[2]):"";
-        operator == "NumpadMultiply" ? numero[0] = this.multiplicar(numero[1],numero[2]):"";
-        operator == "NumpadDivide" ? numero[0] = this.dividir(numero[1],numero[2]):"";
-        console.log("%c RESULTADO -> "+numero[0],"color:lightgreen")
+        operator == "+" ? numero[0] = this.sumar(numero[1],numero[2]):"";
+        operator == "-" ? numero[0] = this.restar(numero[1],numero[2]):"";
+        operator == "/" ? numero[0] = this.dividir(numero[1],numero[2]):"";
+        operator == "*" ? numero[0] = this.multiplicar(numero[1],numero[2]):"";
+        console.log("%c -- RESULTADO -> "+numero[0],"color:lightgreen")
         this.mostrar(numero[0])
         numero[1] = numero[0];
         numero[2] = 0;
         return numero
     },
-    mostrar(a){
-        display.textContent = a; 
+    detecTecla(tecla){
+        //-------- Logica de numeros
+        console.log("%cTecla: "+tecla,"color:yellow")
+        if(tecla >= 0 && tecla <= 9){
+            numero[idx] += tecla
+            numero[idx] = parseFloat(numero[idx])
+            this.mostrar(numero[idx]);
+        }    
+        //-------- Logica de botones
+        if(tecla == "=" || tecla == "Enter" || tecla == "+" || tecla == "-" || tecla == "*" || tecla == "/"){ 
+            if (operador != tecla && idx == 2 && numero[2] != 0 && tecla != "Enter"){ 
+                numero = this.calcular(operador, numero)
+                operador = tecla     
+            }else if ((idx == 2 && numero[2] != 0) || tecla == "Enter"){
+                numero = this.calcular(operador, numero)
+            }else if(idx == 1){
+                operador = tecla
+                idx = 2;
+            }else{
+                operador = tecla
+            }
+        }
+        console.log("----- %cNumero 1: "+numero[1]+"  -- Numero 2: "+numero[2],"color:lightblue","  -- Buffer: "+numero[0]); 
+    },
+    mostrar(num){
+        display.textContent = num; 
     },
 }
 
-//-------- Evento cuando se presiona el teclado 
-calculadora.addEventListener("keydown", e => { 
-    if(e.key >= 0 && e.key <= 9){
-        numero[idx] += e.key
-        numero[idx] = parseFloat(numero[idx])
-        alu.mostrar(numero[idx]);
-    }
+// ============ Liteners ===========
+calculadora.addEventListener("keydown", e => {
+    e.preventDefault()
+    alu.detecTecla(e.key)
+});
+
+teclado.addEventListener("click", e => {
+    e.preventDefault()
+    alu.detecTecla(e.target.value)
+});
+
     
-//-------- Logica de botones - teclado
-    console.log("Numero 1: "+numero[1]+"  -- Numero 2: "+numero[2]+"  --%c Operador: "+e.code,"color:yellow","  -- Buffer: "+numero[0]);
-    if(e.code == "NumpadAdd" || e.code == "NumpadSubtract" || e.code == "NumpadMultiply" || e.code == "NumpadDivide" || e.code == "NumpadEnter"){
-        if (operador != e.code && idx == 2 && numero[2] != 0 && e.code != "NumpadEnter"){ 
-            numero = alu.calcular(operador, numero)
-            operador = e.code     
-        }else if ((idx == 2 && numero[2] != 0) || e.code == "NumpadEnter"){
-            numero = alu.calcular(operador, numero)
-        }else if(idx == 1){
-            operador = e.code
-            idx = 2;
-        }else{
-            operador = e.code
-        }
-    }
-})
-
-
-// NumpadDivide
-// NumpadMultiply
-// NumpadSubtract
-// NumpadAdd
-// NumpadEnter
-
-
-// if (e.code == "NumpadAdd"){
-    //     idx = 2
-    //     numero[0] = alu.sumar(numero[1],numero[2])
-    //     numero[1] = numero[0]
-    //     numero[2] = 0
-    //     console.log("Dentro de Sumar")
-    // }
-    // if (e.code == "NumpadSubtract"){
-    //     idx = 2
-    //     numero[0] = alu.restar(numero[1],numero[2])
-    //     numero[1] = numero[0]
-    //     numero[2] = 0
-    //     console.log("Dentro de Restar")
-    // }
