303440f6049a886c20c8db60b2b785fd5e1353b9
Leopoldo <lnnemec@gmail.com>
2024-01-05 19:34:43 +0100

    Agregado manejo de numeros pequeños

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 let calculadora = document.querySelector("*")
 let display = document.querySelector("#lcd")
 let teclado = document.querySelector(".teclado")
-let numero = [0,"",0]
+let numero = ["","",""]
 let idx = 1
 let operador = ""
 
@@ -19,15 +19,41 @@ const alu = {
     dividir(a,b){
         return a/b
     },
-    calcular(operator,numero){
+    contieneDecimales(numero){
+        let decimalesRetorno = [];
+        numero = numero.map((elemento, indice) => {
+            elemento = elemento.toString()
+            // console.log("Indice: ", indice)
+            // console.log("Numero: ", elemento)
+            // console.log("Contiene Decimales (numero % 1) -> ", elemento % 1)
+            // console.log("Length: ", elemento.length)
+            // console.log("indexOf: ", elemento.indexOf("."))
+            elemento % 1 != 0 ? decimalesRetorno[indice] = (elemento.length-1) - elemento.indexOf(".") : decimalesRetorno[indice] = 1;            
+        })
+        //console.table(decimalesRetorno)
+        if(decimalesRetorno[1] > decimalesRetorno[2]){
+            return decimalesRetorno[1]
+         }
+         return decimalesRetorno[2];
+    },
+    calcular(operator,numero){  
+        // control para numeros pequeños
+        let multiplicador = 10
+        let decimales = this.contieneDecimales(numero)
+        numero = numero.map(numero=>parseFloat(numero))
+        multiplicador **= decimales
+        numero = numero.map(elemento => elemento*multiplicador);
+        
+        // operaciones
         operator == "+" ? numero[0] = this.sumar(numero[1],numero[2]):"";
         operator == "-" ? numero[0] = this.restar(numero[1],numero[2]):"";
         operator == "/" ? numero[0] = this.dividir(numero[1],numero[2]):"";
         operator == "*" ? numero[0] = this.multiplicar(numero[1],numero[2]):"";
+        numero[0] /= multiplicador
         console.log("%c -- RESULTADO -> "+numero[0],"color:lightgreen")
         this.mostrar(numero[0])
         numero[1] = numero[0];
-        numero[2] = 0;
+        numero[2] = "";
         return numero
     },
     detecTecla(tecla){
@@ -38,11 +64,10 @@ const alu = {
             let decimal = numero[idx].length
             if(numero[idx].indexOf(".") == -1){
                 console.log("Convirtiendo a numero - 1er IF")
-                numero[idx] = parseFloat(numero[idx])
+                //numero[idx] = parseFloat(numero[idx])
             }else if(numero[idx][decimal-1] != "0"){
                     console.log("Convirtiendo a numero")
-                    numero[idx] = parseFloat(numero[idx])
-                    // numero[idx] = Number(numero[idx].toFixed())
+                    //numero[idx] = parseFloat(numero[idx])
             }
             this.mostrar(numero[idx]);
         }
